3c56678e58ca6957ad556e86c70f881d1ce976bd
Lim Yun Ching <yunching.lim@gmail.com>
2024-01-11 20:33:16 +0800

    Add logging functionality and remove test.py

---
+++ poc_logging.py
@@ -0,0 +1,8 @@
+# https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
+
+import logging
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
